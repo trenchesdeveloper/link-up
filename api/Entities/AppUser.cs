4777b9fe456e7391e6d66cf03eaed390a954e147
trenchesdeveloper <opeyemisamuel222@gmail.com>
2024-10-27 17:33:45 +0100

    add password and hashing to api

--- api/Entities/AppUser.cs
+++ api/Entities/AppUser.cs
@@ -5,6 +5,8 @@ public class AppUser
     public int Id { get; set; }
     
     public required string Username { get; set; }
-    
-    
+
+    public required byte[] PasswordHash { get; set; }
+
+    public required byte[] PasswordSalt { get; set; }
 }
